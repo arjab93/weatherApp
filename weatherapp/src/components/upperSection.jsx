497773be201062068f8b9fbf86d75323e0ff515f
Arjab Khadka <arjabkhadka93@gmail.com>
2024-04-01 23:22:38 +0545

    update: changes made to make it dynamic

---
+++ weatherapp/src/components/upperSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const upperSection = ({ place, temp }) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      {/* place */}
+      <div className="text-[70px]">
+        {place}
+      </div>
+      {/* temp */}
+      <div className='text-[180px] my-[-80px] mb-[-70px]'>
+        {temp}&deg;C
+      </div>
+      {/* date */}
+      <div className='text-[30px]'>
+        {[year, month, day].join("-")}
+      </div>
+    </div>
+  )
+}
+
+export default upperSection
