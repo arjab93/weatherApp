146f31b06f8b7ecbaa16eca3da35136ef16e56a5
Arjab Khadka <arjabkhadka93@gmail.com>
2024-04-03 13:34:25 +0545

    update: router added

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
 import MainSection from './components/mainSection';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
-  //const [state, setState] = useState(null)
   const [weatherData, setWeatherData] = useState(null);
   const [location, setLocation] = useState("");
   const [submitted, setSubmitted] = useState(false);
@@ -18,38 +18,42 @@ const App = () => {
     } catch (error) {
       console.log("Error fetching the data.");
     }
-  }
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  };
 
   return (
-    // <div>
-    //   {weatherData ? (
-    //     <div classname="">
-    //       <li>Pressure: {weatherData.main.pressure} atm</li>
-    //       <li>Temperature: {weatherData.main.temp} &deg;K</li>
-    //     </div>
-    //   ): (
-    //     <p>Loading Data</p>
-    //   )}
-    // </div>
-    <div className='h-screen bg-[#141316] font-Changa flex flex-col justify-center items-center'>
-      <div className="mb-3 w-[500px] text-[25px] font-changa flex justify-between items-center">
-        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} className='px-4 py-3 w-[70%] outline-none border-none rounded-md' />
-        <button type='submit' className='py-3 bg-blue-500 text-white font-changa w-[28%] outline-none border-none rounded-md' onClick={() => fetchData()}>SUBMIT</button>
-      </div>
-      {submitted ? (
-        location ? (
-          <MainSection place={location} data={weatherData} />
-        ) : (
-          <p className='text-red-400'><sup>*</sup>Please Enter your location</p>
-        )
-      ) : null}
+    <Router>
+      <div className='h-screen bg-[#141316] font-Changa flex flex-col justify-center items-center'>
+        <div className="mb-3 w-[500px] text-[25px] font-changa flex justify-between items-center">
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className='px-4 py-3 w-[70%] outline-none border-none rounded-md'
+          />
+          <button
+            type='submit'
+            className='py-3 bg-blue-500 text-white font-changa w-[28%] outline-none border-none rounded-md'
+            onClick={() => fetchData()}
+          >
+            SUBMIT
+          </button>
+        </div>
 
-    </div>
-  )
-}
+        <Routes>
+          <Route exact path="/" element={
+            submitted ? (
+              location ? (
+                <MainSection place={location} data={weatherData} />
+              ) : (
+                <p className='text-red-400'><sup>*</sup>Please Enter your location</p>
+              )
+            ) : null
+          } />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
